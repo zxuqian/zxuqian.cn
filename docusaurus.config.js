7f6f3f0aa7635f4de0585fd73a1e756a8f079369
zxuqian <zxuqian@gmail.com>
2020-06-10 12:32:37 +0800

    Fix a breaking change

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -116,7 +116,7 @@ module.exports = {
       },
     ],
   ],
-  themes: ["@docusaurus/theme-live-codeblock"],
+  themes: [require.resolve("@docusaurus/theme-live-codeblock")],
   plugins: [
     path.resolve(__dirname, "./src/plugin/plugin-baidu-analytics"),
     path.resolve(__dirname, "./src/plugin/plugin-google-adsense"),
