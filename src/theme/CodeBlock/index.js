13daf97db1b37b3f87011a5e1402c56e410a15f4
zxuqian <zxuqian@gmail.com>
2020-06-10 12:50:40 +0800

    Update codeblock

--- src/theme/CodeBlock/index.js
+++ src/theme/CodeBlock/index.js
@@ -1,127 +1,37 @@
 /**
- * Copyright (c) 2017-present, Facebook, Inc.
+ * Copyright (c) Facebook, Inc. and its affiliates.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
 
-import React, { useEffect, useState, useRef } from "react";
-import classnames from "classnames";
-import Highlight, { defaultProps } from "prism-react-renderer";
-import defaultTheme from "prism-react-renderer/themes/palenight";
-import Clipboard from "clipboard";
-import rangeParser from "parse-numeric-range";
+import React from "react";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import usePrismTheme from "@theme/hooks/usePrismTheme";
 import Playground from "@theme/Playground";
-import styles from "./styles.module.css";
+import CodeBlock from "@theme-init/CodeBlock";
 import styled from "styled-components";
 
-const highlightLinesRangeRegex = /{([\d,-]+)}/;
+const withLiveEditor = (Component) => {
+  const WrappedComponent = (props) => {
+    const { isClient } = useDocusaurusContext();
+    const prismTheme = usePrismTheme();
 
-export default ({
-  children,
-  className: languageClassName,
-  live,
-  metastring,
-  ...props
-}) => {
-  const {
-    siteConfig: {
-      themeConfig: { prism = {} }
-    }
-  } = useDocusaurusContext();
-  const [showCopied, setShowCopied] = useState(false);
-  const target = useRef(null);
-  const button = useRef(null);
-  let highlightLines = [];
-
-  if (metastring && highlightLinesRangeRegex.test(metastring)) {
-    const highlightLinesRange = metastring.match(highlightLinesRangeRegex)[1];
-    highlightLines = rangeParser.parse(highlightLinesRange).filter(n => n > 0);
-  }
-
-  useEffect(() => {
-    let clipboard;
-
-    if (button.current) {
-      clipboard = new Clipboard(button.current, {
-        target: () => target.current
-      });
+    if (props.live) {
+      return (
+        <Playground
+          key={isClient}
+          scope={{ ...React, styled }}
+          theme={prismTheme}
+          {...props}
+        />
+      );
     }
 
-    return () => {
-      if (clipboard) {
-        clipboard.destroy();
-      }
-    };
-  }, [button.current, target.current]);
-
-  if (live) {
-    return (
-      <Playground
-        scope={{ ...React, styled }}
-        code={children.trim()}
-        theme={prism.theme || defaultTheme}
-        {...props}
-      />
-    );
-  }
-
-  let language =
-    languageClassName && languageClassName.replace(/language-/, "");
-
-  if (!language && prism.defaultLanguage) {
-    language = prism.defaultLanguage;
-  }
-
-  const handleCopyCode = () => {
-    window.getSelection().empty();
-    setShowCopied(true);
-
-    setTimeout(() => setShowCopied(false), 2000);
+    return <Component {...props} />;
   };
 
-  return (
-    <Highlight
-      {...defaultProps}
-      theme={prism.theme || defaultTheme}
-      code={children.trim()}
-      language={language}
-    >
-      {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <div className={styles.codeBlockWrapper}>
-          <pre
-            ref={target}
-            className={classnames(className, styles.codeBlock)}
-            style={style}
-          >
-            {tokens.map((line, i) => {
-              const lineProps = getLineProps({ line, key: i });
-
-              if (highlightLines.includes(i + 1)) {
-                lineProps.className = `${lineProps.className} docusaurus-highlight-code-line`;
-              }
-
-              return (
-                <div key={i} {...lineProps}>
-                  {line.map((token, key) => (
-                    <span key={key} {...getTokenProps({ token, key })} />
-                  ))}
-                </div>
-              );
-            })}
-          </pre>
-          <button
-            ref={button}
-            type="button"
-            aria-label="Copy code to clipboard"
-            className={styles.copyButton}
-            onClick={handleCopyCode}
-          >
-            {showCopied ? "Copied" : "Copy"}
-          </button>
-        </div>
-      )}
-    </Highlight>
-  );
+  return WrappedComponent;
 };
+
+export default withLiveEditor(CodeBlock);
