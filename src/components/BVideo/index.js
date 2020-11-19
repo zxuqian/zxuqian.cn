4844e8613650d1cb96de0b57d2cf6e5e47682e0e
Xuqian Zhang <zxuqian@gmail.com>
2020-11-19 13:27:23 +0800

    Update code

--- src/components/BVideo/index.js
+++ src/components/BVideo/index.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
 function BVideo({ src, bsrc }) {
+  // get aid
+  // const params = new URLSearchParams(src.split("?")[1]);
+  // const aid = params.get("aid");
+
+  // const [view, setView] = useState("");
+
+  // const getView = async () => {
+  //   const resp = await fetch(
+  //     `https://api.bilibili.com/archive_stat/stat?aid=${aid}`
+  //   );
+  //   const data = await resp.json();
+  //   const { view } = data?.data;
+  //   setView(view);
+  // };
+
+  // useEffect(() => {
+  //   getView();
+  // }, []);
+
   return (
     <>
       <iframe
@@ -13,6 +32,9 @@ function BVideo({ src, bsrc }) {
         allowFullScreen={true}
         style={{ width: "100%", height: "400px" }}
       ></iframe>
+      {/* <div>
+        <p>播放量：{view}</p>
+      </div> */}
       <div style={{ fontWeight: 800, padding: "2em 0" }}>
         <a href={bsrc}>点击跳转到B站进行观看~记得给个三连哦！</a>
       </div>
