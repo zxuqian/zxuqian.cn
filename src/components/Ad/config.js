755506506ee97a5d8c1254ec2daecbd0002b9c82
Xuqian Zhang <zxuqian@gmail.com>
2021-03-12 12:56:29 +0800

    Minor changes

--- src/components/Ad/config.js
+++ src/components/Ad/config.js
@@ -4,15 +4,24 @@ export default {
       id: "sidebarAd1",
       alt: "【腾讯云】云产品限时秒杀，爆款1核2G云服务器，首年99元",
       imageSrc: "img/ads/sidebar_ad1.webp",
-      link: "https://curl.qcloud.com/wHjeQakV",
+      link:
+        "https://cloud.tencent.com/act/cps/redirect?redirect=1077&cps_key=2c427bf21a8dbca51ee45224ab31c2a9&from=console",
+    },
+    {
+      id: "sidebarAd2",
+      alt: "采购季云小站狂欢节，上云采购三重奖励拿不停！",
+      imageSrc:
+        "https://img.alicdn.com/imgextra/i1/O1CN01xmAfRY1chVAa3L9vn_!!6000000003632-0-tps-300-200.jpg",
+      link: "https://www.aliyun.com/minisite/goods?userCode=idwbk8q2",
     },
   ],
   articleFooter: [
     {
       id: "articleFooterAd1",
-      alt: "腾讯云】云产品限时秒杀，爆款1核2G云服务器，首年99元",
+      alt: "【腾讯云】云产品限时秒杀，爆款1核2G云服务器，首年99元",
       imageSrc: "img/ads/post_footer_ad1.webp",
-      link: "https://curl.qcloud.com/wHjeQakV",
+      link:
+        "https://cloud.tencent.com/act/cps/redirect?redirect=1077&cps_key=2c427bf21a8dbca51ee45224ab31c2a9&from=console",
     },
   ],
 };
--- src/components/Ad/index.js
+++ src/components/Ad/index.js
@@ -6,7 +6,11 @@ function Ad({ src, link, alt, text, ...rest }) {
   return (
     <div {...rest}>
       <a href={link} title={alt}>
-        <img src={useBaseUrl(src)} alt={alt} className={styles.adImage} />
+        <img
+          src={src.startsWith("http:") ? src : useBaseUrl(src)}
+          alt={alt}
+          className={styles.adImage}
+        />
       </a>
       {text && <p>{text}</p>}
     </div>
